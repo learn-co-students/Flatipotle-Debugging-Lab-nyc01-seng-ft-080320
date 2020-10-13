0a9be0e32198a0eb4dd53f9b0c817ed56b3955c5
russellsutt <russell.sutter@gmail.com>
2020-10-13 14:36:54 -0400

    Done.

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
+import Side from './Side'
 
 class Order extends Component {
   state = {
     isClicked: false
   }
 
-  handleClick = () => {
-    this.state.isClicked = !this.state.isClicked
+  handleClick = (e) => {
+    this.setState((prevState) => ({ isClicked: !prevState.isClicked}))
   }
 
   render() {
